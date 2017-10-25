e859a7589ce8b70584e77ff434fe36b213c4e93e
Ivan Valkov <i.valkov92@gmail.com>
2017-10-25 23:26:07 +0300

    Configuration

---
+++ app/models/user.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose'); // invoked mongoose modules
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = new Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        maxlength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+    }
+});
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.password, null, null, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+
+})
+
+
+module.exports = mongoose.model('User', UserSchema);
