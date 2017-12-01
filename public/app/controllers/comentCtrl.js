5565463f9bb338d0135ede67c30e8e142b7a7a89
Ivan Valkov <i.valkov92@gmail.com>
2017-12-01 10:45:14 +0200

    little correction

--- public/app/controllers/comentCtrl.js
+++ public/app/controllers/comentCtrl.js
@@ -1,30 +1,34 @@
 var app = angular.module("detailsController", ['authServices']);
 app.controller("comentController", ["Auth", "$scope", "$http", "$location", "$routeParams", function(Auth, $scope, $http, $location, $routeParams) {
-
-    if (Auth.isLoggedIn()) {
-        Auth.getUser().then(function(response) {
-            var userObject = response.data;
-            //show all coments
-            $scope.getComents = function() {
-                $http.get("/coments").then(function(response) {
-                    var currentBookId = $routeParams.id;
-                    $scope.coments = response.data.filter(x => x.bookId == currentBookId);;
+    //show all coments
+    $scope.getComents = function() {
+        $http.get("/coments").then(function(response) {
+            var currentBookId = $routeParams.id;
+            $scope.coments = response.data.filter(x => x.bookId == currentBookId);
+            if (Auth.isLoggedIn()) {
+                Auth.getUser().then(function(response) {
+                    var userObject = response.data;
                     $scope.comentsFromUser
                 })
             }
+        })
+    }
 
-            //show current User All comments
-            $scope.getComentFromCurrentUser = function() {
-                $http.get("/coments").then(function(response) {
-                    console.log(userObject)
-                    console.log(response)
-                    $scope.coments = response.data.filter(x => x.author.id == userObject.id)
-                })
-            }
-            $scope.addComent = function() {
-                var currentBookId = $routeParams.id;
-                var textFromInput = document.querySelector("#comentar").value;
-
+    //show current User All comments
+    $scope.getComentFromCurrentUser = function() {
+        Auth.getUser().then(function(response) {
+            var currentUserObject = response.data;
+            $http.get("/coments").then(function(response) {
+                $scope.coments = response.data.filter(x => x.author.id == currentUserObject.id)
+            })
+        })
+    }
+    $scope.addComent = function() {
+        var currentBookId = $routeParams.id;
+        var textFromInput = document.querySelector("#comentar").value;
+        if (Auth.isLoggedIn()) {
+            Auth.getUser().then(function(response) {
+                var userObject = response.data;
                 function Coment(author, bookId, text) {
                     this.author = author;
                     this.bookId = bookId;
@@ -47,32 +51,37 @@ app.controller("comentController", ["Auth", "$scope", "$http", "$location", "$ro
                 $http.post("/coments", $scope.coment).then(function(response) {
                     $scope.getComents();
                 })
-                setTimeout(function() {
-                    textFromInput = document.querySelector("#comentar").value = "";
-                }, 1000);
-            }
-            $scope.deleteComent = function(id) {
-                    $http.delete("/coments/" + id).then(function(response) {
-                        $scope.getComentFromCurrentUser();
-                    })
-                }
-                // za like-ovete
-            $scope.editComent = function(id) {
-                $http.get("/coments/" + id).then(function(response) {
-                    $scope.coment = response.data;
-                    if ($scope.coment.likes.userId.find(id => id == userObject.id)) {
+            })
+        } else {
+            $location.path('/login');
+        }
+        setTimeout(function() {
+            textFromInput = document.querySelector("#comentar").value = "";
+        }, 1000);
+    }
+    $scope.deleteComent = function(id) {
+            $http.delete("/coments/" + id).then(function(response) {
+                $scope.getComentFromCurrentUser();
+            })
+        }
+        // za like-ovete
+    $scope.editComent = function(id) {
+        $http.get("/coments/" + id).then(function(response) {
+            $scope.coment = response.data;
+            
+            if (Auth.isLoggedIn()) {
+                Auth.getUser().then(function(response) {
+                    var userObject = response.data;
+                    if ($scope.coment.likes.userId.find(id=>id == userObject.id)){
                         throw new Error("Veche si haresal")
-                    } else {
+                    }else{
                         $scope.coment.likes.userId.push(userObject.id);
                         $scope.coment.likes.like++;
                         $http.put("/coments/" + id, $scope.coment).then(function(response) {
                             $scope.getComents();
                         })
-                    }
-                })
-            }
+                    }                    
+                })}
         })
-    } else {
-        $location.path('/login');
     }
 }]);
